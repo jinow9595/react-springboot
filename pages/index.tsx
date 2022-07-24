08e1d8c51f6b858a641710e0fb26b8d1e7b0b8b4
jinow9595 <jinow9595@gmail.com>
2022-07-24 22:24:58 +0900

    typescript, main appbar 추가

---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import type { NextPage } from 'next'
+import CustomAppBar from '../component/CustomAppBar'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <CustomAppBar />
+      <CustomAppBar />
+    </>
+  )
+}
+
+export default Home
